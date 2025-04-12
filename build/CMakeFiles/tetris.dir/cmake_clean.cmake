c165f7406c5f980df4886c22538071a971785de0
a-ia <sharonsudan@outlook.com>
2025-04-11 22:20:38 -0400

    first commit

---
+++ build/CMakeFiles/tetris.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tetris.dir/link.d"
+  "CMakeFiles/tetris.dir/src/Button.cpp.o"
+  "CMakeFiles/tetris.dir/src/Button.cpp.o.d"
+  "CMakeFiles/tetris.dir/src/Game.cpp.o"
+  "CMakeFiles/tetris.dir/src/Game.cpp.o.d"
+  "CMakeFiles/tetris.dir/src/Piece.cpp.o"
+  "CMakeFiles/tetris.dir/src/Piece.cpp.o.d"
+  "CMakeFiles/tetris.dir/src/main.cpp.o"
+  "CMakeFiles/tetris.dir/src/main.cpp.o.d"
+  "tetris"
+  "tetris.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tetris.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
